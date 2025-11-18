b4d069d6d6dee297b32c51b55e62feb4f02f9f8b
diego5714 <diego5714@proton.me>
2025-11-18 02:12:52 -0300

    Add custom keyboard layout to system image.

---
+++ files/scripts/register_keyboard_layouts.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+echo "Registering custom keyboard layout 'us-custom'..."
+
+# Modify evdev.lst
+sed -i '/^  us              English (US)$/a \  us-custom       English (US, Custom)' /usr/share/X11/xkb/rules/evdev.lst
+
+# Modify evdev.xml
+sed -i '/<\/layoutList>/i \
+    <layout>\
+      <configItem>\
+        <name>us-custom</name>\
+        <shortDescription>US-Custom</shortDescription>\
+        <description>English (US, Custom)</description>\
+        <languageList><iso639Id>eng</iso639Id></languageList>\
+      </configItem>\
+    </layout>' /usr/share/X11/xkb/rules/evdev.xml
+
+echo "Custom keyboard layout 'us-custom' registered succesfully."
